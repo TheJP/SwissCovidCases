38822021fccd827e1d6b0daf0130a34f5f30fd28
TheJP <jp_55555@hotmail.com>
2020-11-06 11:59:10 +0100

    Add project files.

---
+++ SwissCovidCases/SwissCovidCasesService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissCovidCases
+{
+    public partial class SwissCovidCasesService : ServiceBase
+    {
+        public SwissCovidCasesService()
+        {
+            InitializeComponent();
+            if (!EventLog.SourceExists(eventLog.Source))
+            {
+                EventLog.CreateEventSource(eventLog.Source, eventLog.Log);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("Service Start", EventLogEntryType.Information);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("Service Stop", EventLogEntryType.Information);
+        }
+    }
+}
